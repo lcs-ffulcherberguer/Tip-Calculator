e8d4be41450dff000037688ab1f7e51c486ceb5c
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-10-10 11:17:32 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Fulcherberguer, Fernanda on 2019-10-10.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
