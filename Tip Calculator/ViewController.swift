6c0a66986a46b07aa42ef951bd98ddf034c838ff
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-10-21 15:31:24 -0400

    Created buttons

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -12,49 +12,82 @@ class ViewController: UIViewController {
 
     //MARK: Properties
     
+    //Button Pressed as a false boolean
+    var buttonPressed: Bool = false
+    //Tip Percent as a double data type with decimal places
+    var tipPercent = Double (0.0)
     
-    @IBOutlet weak var totalAmountLabel: UITextField!
     
-    @IBOutlet weak var tipAmountLabel: UITextField!
+    
+    
+    
+    
+  
+    @IBOutlet weak var totalAmount: UILabel!
     
     @IBOutlet weak var billAmountTextField: UITextField!
     
     @IBOutlet weak var splitBetweenTextField: UITextField!
     
-    @IBOutlet weak var amountPerpersonLabel: UITextField!
+    @IBOutlet weak var amountPerPerson: UILabel!
+    
+    @IBOutlet weak var errorLabel: UILabel!
     
     
+    //MARK: Methods
     
     
+    //Runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
     }
 
-    @IBAction func calculateTip(_ sender: Any) {
-        
-        let billAmount = Double(self.billAmountTextField.text ?? "") ?? 0
-        let tipPercentage = 0.20
-        
-        let tipAmount =  (billAmount * tipPercentage)
-        self.tipAmountLabel.text = "$\(tipAmount)"
-        
-        let total = (billAmount + tipAmount)
-        self.totalAmountLabel.text = "$\(total)"
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    @IBAction func tip5(_ sender: Any) {
+        tipPercent = 0.05
+        buttonPressed = true
     }
     
+    @IBAction func tip10(_ sender: Any) {
+        tipPercent = 0.10
+        buttonPressed = true
+    }
+    
+    @IBAction func tip15(_ sender: Any) {
+        tipPercent = 0.15
+        buttonPressed = true
+    }
+    
+    @IBAction func tip20( sender:Any) {
+        tipPercent = 0.20
+        buttonPressed = true
+    
+    }
+    
+    //  Get the amount of bill value from the text field
+    @IBAction func calculateTip( sender:Any) {
+    
+    //Check if a tip option is selected
+        if buttonPressed == false {
+            errorLabel.text = "Error. Please check all fields are completed."
+            return
+    }
+        
+    //Check if amount of people has a value
+        guard let amountOfBillString = billAmountTextField.text, amountOfBillString != ""  else {
+            errorLabel.text = "Error. Please check all fields are completed."
+            return
+    }
+        
+    //Check if amount of people has a value
+        guard let amountOfPeopleString = splitBetweenTextField.text, amountOfPeopleString != "" else {
+        errorLabel.text = "Error. Please check all fields are completed."
+        return
+        
+    }
+        
+    
+        
+    
 }
-
+}
